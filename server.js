2feee46f61a5981ca65db22356d59ad07663d984
Adit Chandra <aditchandra@college.harvard.edu>
2016-07-17 23:57:11 -0700

    stringify json for smooch call

--- server.js
+++ server.js
@@ -38,7 +38,9 @@ app.get('/', function(req, res){
 });
 
 function user_sleep(userId) {
-  var state_stop =  {'properties': '{"state": "stop"}'};
+  var state_stop =  {'properties': {
+                      {'state': 'stop'}
+                    };
   request({
     method: 'PUT',
     url: smooch_uri + userId,
@@ -46,7 +48,7 @@ function user_sleep(userId) {
       'app-token' : smooch_app_token
     },
     dataType: 'json',
-    data: state_stop
+    data: JSON.stringify(state_stop)
   }, function(error, response, body){
     console.log('SMOOCH PUT REQUEST RETURNED BODY: ');
     console.log(body);
