a80a99fed9b34e4c02f343c114969aeb0484a44b
Adit Chandra <aditchandra@college.harvard.edu>
2016-07-18 00:52:18 -0700

    trying curl

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+var curl = require('curlrequest');
 
 var app = express();
 
@@ -38,30 +39,47 @@ app.get('/', function(req, res){
 });
 
 function user_sleep(userId) {
-  var state_stop = '{"properties":"{\"state\":\"stop\"}"}';
-  request({
+  // var state_stop = '{"properties":"{\"state\":\"stop\"}"}';
+  // request({
+    // method: 'PUT',
+    // url: smooch_uri + userId,
+    // headers: {
+    //   'app-token' : smooch_app_token
+    // },
+    // dataType: 'json',
+    // data: state_stop
+  // }, function(error, response, body){
+  //   console.log('ERROR?: ');
+  //   console.log(error);
+  //   console.log('RESPONSE?: ');
+  //   console.log(response);
+  //   console.log('SMOOCH PUT REQUEST RETURNED BODY: ');
+  //   console.log(body);
+  //   return body;
+  // });
+  var state_stop = {'properties' : {'state' : 'stop'}};
+  var options = {
     method: 'PUT',
     url: smooch_uri + userId,
     headers: {
       'app-token' : smooch_app_token
     },
-    dataType: 'json',
     data: state_stop
-  }, function(error, response, body){
+  };
+
+  curl.request(options, function(error, data){
     console.log('ERROR?: ');
     console.log(error);
-    console.log('RESPONSE?: ');
-    console.log(response);
     console.log('SMOOCH PUT REQUEST RETURNED BODY: ');
-    console.log(body);
-    return body;
+    console.log(data);
+    return data;
   });
 }
 
 // set users to sleep
 app.get('/sleep/:userId', function(req, res){
   var userId = req.params.userId;
-  var resp = user_sleep(userId);
+  var resp_data = user_sleep(userId);
   res.send('recieved sleep call!');
 });
 
