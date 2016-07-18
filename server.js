d0aa6f59c3754452928acd71b8ccb7b1c8e892a5
Adit Chandra <aditchandra@college.harvard.edu>
2016-07-18 00:27:15 -0700

    yet another state_stop reformat

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.get('/', function(req, res){
 });
 
 function user_sleep(userId) {
-  var state_stop = {'properties': [{'state': 'stop'}]};
+  var state_stop = {'properties' : {'state' : 'stop'}};
   request({
     method: 'PUT',
     url: smooch_uri + userId,
